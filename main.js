5a224034fb7d75abf8be6d5a29d7499c55bb7220
Harsh <harsh.parekh@aimdek.com>
2025-04-28 11:49:07 +0530

    Learned about variable declaration

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var message = 'Welcome back';
+var message = 'Welcome back'; //Hello World
 console.log(message);
+var x = 10; //Variable declaration
+var y = 30;
+var sum = x + y;
+var title = 'AIMDEK';
--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 export {}
 
-let message = 'Welcome back';
+let message = 'Welcome back'; //Hello World
 console.log(message);
+
+let x = 10;  //Variable declaration
+let y = 30;
+
+let sum = x + y;
+const title = 'AIMDEK';
+
